8288781a846eaa53c2fdc18825ee40f1b330e180
Roince Jumao-as <hindipsg@gmail.com>
2025-05-19 13:13:58 +0800

    Deleted Unnecessary Files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddEmployee from "./pages/AddEmployee";
-import EditEmployee from "./pages/EditEmployee";
 
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
@@ -9,7 +7,6 @@ import AdminLayout from "./layout/AdminLayout";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
 import EmployeeLayout from "./layout/EmployeeLayout";
-import EmployeeHome from "./pages/EmployeeHome";
 import EmployeeProfile from "./pages/EmployeeProfile";
 import AdminProfile from "./pages/AdminProfile";
 import AdminNewUser from "./pages/AdminNewUser";
@@ -37,15 +34,11 @@ function App() {
             <Route path="/admin/profile" element={<AdminProfile />} />
             <Route path="/admin/employees" element={<AdminEmployeeList />} />
             <Route path="/admin/users" element={<AdminAllUsersList />} />
-            {/* <Route path="/admin/departments" element={<Department />} /> */}
-            <Route path="/admin/add-employee" element={<AddEmployee />} />
             <Route path="/admin/add-user" element={<AdminNewUser />} />
-            <Route path="/admin/edit-employee/:id" element={<EditEmployee />} />
             <Route path="/admin/logs" element={<AdminLogs />} />
           </Route>
    
           <Route element={<EmployeeLayout />}>
-            <Route path="/employee/home" element={<EmployeeHome />} />  
             <Route path="/employee/profile" element={<EmployeeProfile />} />
           </Route>
 
